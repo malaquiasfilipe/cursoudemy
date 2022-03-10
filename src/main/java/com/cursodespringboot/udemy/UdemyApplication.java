671ffffbf4dc22f8c754f0cc5174f47b622d086d
Malaquias Victor Filipe <malaquiasphilipe.kilson@gmail.com>
2022-03-11 00:49:13 +0100

    Salvando  a tabela Categoria de forma automatica

--- src/main/java/com/cursodespringboot/udemy/UdemyApplication.java
+++ src/main/java/com/cursodespringboot/udemy/UdemyApplication.java
@@ -1,13 +1,34 @@
 package com.cursodespringboot.udemy;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class UdemyApplication {
+import com.cursodespringboot.udemy.domain.Categoria;
+import com.cursodespringboot.udemy.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class UdemyApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+				
 	}
 
 }
