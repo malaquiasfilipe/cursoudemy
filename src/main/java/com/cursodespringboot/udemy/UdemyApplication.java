89770d5130c16c16431b5bcd18e8c2a44f30f41e
Malaquias Victor Filipe <malaquiasphilipe.kilson@gmail.com>
2022-03-11 15:15:08 +0100

    Productos e associação muitos para muitos

--- src/main/java/com/cursodespringboot/udemy/UdemyApplication.java
+++ src/main/java/com/cursodespringboot/udemy/UdemyApplication.java
@@ -1,5 +1,6 @@
 package com.cursodespringboot.udemy;
 
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cursodespringboot.udemy.domain.Categoria;
+import com.cursodespringboot.udemy.domain.Produto;
 import com.cursodespringboot.udemy.repositories.CategoriaRepository;
+import com.cursodespringboot.udemy.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class UdemyApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyApplication.class, args);
@@ -25,7 +31,19 @@ public class UdemyApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2200.00);
+		Produto p2 = new Produto (null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 50.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		
 				
