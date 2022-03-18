9e48fecf36bed6827ee7cd092aad5e16b674b2ef
Malaquias Victor Filipe <malaquiasphilipe.kilson@gmail.com>
2022-03-18 23:44:53 +0100

    Trabalhando com PUT(atualizando a Categoria) e alguns pequenos ajustes no programa

--- src/main/java/com/cursodespringboot/udemy/services/PedidoService.java
+++ src/main/java/com/cursodespringboot/udemy/services/PedidoService.java
@@ -15,7 +15,7 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNoFoundException("Objecto não encontrado Id: " + id + ", do tipo "+ Pedido.class.getName()));
